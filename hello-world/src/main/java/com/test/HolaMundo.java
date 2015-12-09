60c96c977c5a36480d0b2c8cac8b5a320b61f17e
villared <villared@globalhitss.com>
2015-12-09 16:09:46 -0600

    Modifica Saludo
    Se modifica la forma en que se obtiene el saludo

--- hello-world/src/main/java/com/test/HolaMundo.java
+++ hello-world/src/main/java/com/test/HolaMundo.java
@@ -6,8 +6,23 @@ package com.test;
  */
 public class HolaMundo 
 {
+    private String saludo = null;
+	
+	public HolaMundo(String saludo){
+		setSaludo(saludo);
+	}
+    
     public static void main(String[] args)
     {
-        System.out.println("Hola Mundo de Git y Maven!");
+        System.out.println(new HolaMundo("Hola Mundo de Git y Maven!"));
     }
-}
+	
+	public String getSaludo() {
+		return saludo;
+	}
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+	
+	
+}//fin de class
