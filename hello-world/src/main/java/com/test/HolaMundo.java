3ff6e1c244f01af357d1aeb65d367e81e8842443
villared <villared@globalhitss.com>
2015-12-09 14:56:36 -0600

    Hola Mundo
    Proyecto inicial de prueba de maven y git

---
+++ hello-world/src/main/java/com/test/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class HolaMundo 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
